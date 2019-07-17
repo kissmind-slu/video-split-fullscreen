ca35729dc22680659c3b9cba91dde648021e114d
Guillermo <guillermo@emprendeaprendiendo.com>
2019-07-17 10:13:31 +0200

    scroll tests

--- index.js
+++ index.js
@@ -145,9 +145,11 @@ if(platform === iphoneMachines[0] || platform === iphoneMachines[1] || platform 
 //scroll exit
 
 window.addEventListener('scroll', function() { 
-    var rect = iframe.getBoundingClientRect();
-    alert(rect.x);
- });
+    if(vsIsFullScreen){
+        var rect = iframe.getBoundingClientRect();
+        alert(rect.x);
+    }
+});
 
 ///testing use case
 wrapper.style.height = iframe.style.height;
